782ef0ab588abe49cd12492ceafd2b6585d96905
Francisco Abayon <francisco.abayon@rafi.org.ph>
2019-02-23 15:02:02 +0800

    Add server environment confiugration
    add production, testing , development setup

--- xx_app/controllers/Welcome.php
+++ xx_app/controllers/Welcome.php
@@ -21,6 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		// $this->load->view('welcome_message');
-		echo 'this is development';
+		// echo 'this is development';
+		echo ENVIRONMENT;
 	}
 }
